793ae8c2e99b578a8dad1661535d3afbb1c21e6c
noberk <noberkli@gmail.com>
2018-08-03 14:56:45 +0800

    删除一个插件

--- Project_Found/src/ts/main.js
+++ Project_Found/src/ts/main.js
@@ -1,3 +1,4 @@
+/// <reference path="../@types/jquery/index.d.ts" />
 var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
     var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
     if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
@@ -11,13 +12,17 @@ function Component(content) {
             content.files.forEach(function (file) {
                 var element = undefined;
                 if (file.endsWith("css") === true) {
-                    console.log('css');
                     element = doElement("link");
                     element.href = file;
                     element.rel = 'stylesheet';
                 }
+                else if (file.endsWith("less") === true) {
+                    element = doElement("link");
+                    element.href = file;
+                    element.type = "text/css";
+                    element.rel = 'stylesheet/less';
+                }
                 else if (file.endsWith("js") === true) {
-                    console.log('js');
                     element = doElement("script");
                     element.src = file;
                 }
@@ -28,6 +33,7 @@ function Component(content) {
                     element.rel = "shortcut icon";
                 }
                 if (element) {
+                    console.log(file + " was loaded");
                     head.appendChild(element);
                 }
             });
@@ -39,30 +45,61 @@ function doElement(name) {
 }
 var Main = /** @class */ (function () {
     function Main() {
-        $(function () {
-            $(".owl-carousel").owlCarousel({
-                items: 1,
-                loop: true,
-                autoplay: true,
-                autoplayTimeout: 3000,
-                autoplayHoverPause: true
-            });
-        });
     }
+    Main.prototype.init = function () {
+        var app = angular.module("body", ['ng']);
+        app.controller("bodyController", function ($scope) {
+            $scope.table = [
+                { month: "3", monthKey: "个月", rate: "3.50%", total: "1,880" },
+                { month: "3-6", monthKey: "个月", rate: "3.60%", total: "2,880 " },
+                { month: "6-12", monthKey: "个月", rate: "4.50%", total: "8,880" },
+                { month: "12-24", monthKey: "个月", rate: "5.60%", total: "5,880" },
+                { month: "24", monthKey: "个月以上", rate: "6.50%", total: "91,080" },
+            ];
+            $scope.table2 = [
+                { month: "3", monthKey: "个月", monthNotice: "保健消费", rate: "4.90%", total: "18,000", button: "投标" },
+                { month: "5", monthKey: "个月", monthNotice: "资金周转", rate: "5.20%", total: "28,880 ", button: "投标" },
+                { month: "7", monthKey: "个月", monthNotice: "个人消费", rate: "5.50%", total: "38,880", button: "还款中" },
+                { month: "1", monthKey: "个月", monthNotice: "日常生活消费", rate: "4.60%", total: "59,880", button: "还款中" },
+            ];
+            $scope.safe = [
+                {
+                    name: "安全", values: [
+                        { Record: "国家AAA信用平台" },
+                        { Record: "银行资金托管" },
+                        { Record: "上市公司北京保证" }
+                    ]
+                },
+                {
+                    name: "权威", values: [
+                        { Record: "国家AAA信用平台" },
+                        { Record: "银行资金托管" },
+                        { Record: "上市公司北京保证" }
+                    ]
+                },
+                {
+                    name: "省心", values: [
+                        { Record: "1000元起头" },
+                        { Record: "用户利益保障机制" },
+                        { Record: "保险公司承保" }
+                    ]
+                },
+            ];
+        });
+    };
     Main = __decorate([
         Component({
             files: [
-                './src/js/vendor/OwlCarousel2-2.2.1/dist/assets/owl.carousel.min.css',
-                './src/js/vendor/OwlCarousel2-2.2.1/dist/assets/owl.theme.default.min.css',
                 './src/css/bootstrap.css',
                 './src/css/index.css',
-                './src/js/vendor/OwlCarousel2-2.2.1/docs/assets/vendors/jquery.min.js',
+                './src/js/less.min.js',
+                './src/js/jquery.min.js',
                 './src/js/bootstrap.js',
-                './src/js/angular.min.js',
                 './src/img/favicon.ico'
             ]
         })
     ], Main);
     return Main;
 }());
-new Main();
+var main = new Main();
+main.init();
--- Project_Found/src/ts/main.ts
+++ Project_Found/src/ts/main.ts
@@ -1,3 +1,4 @@
+/// <reference path="../@types/jquery/index.d.ts" />
 
 interface HeaderContent {
     files: string[];
@@ -10,16 +11,24 @@ function Component(content: HeaderContent) {
             content.files.forEach((file: any) => {
                 let element = undefined;
                 if (file.endsWith("css") === true) {
-                    console.log('css');
+
                     element = doElement("link");
                     element.href = file;
                     element.rel = 'stylesheet';
 
-                } else if (file.endsWith("js") === true) {
-                    console.log('js');
+                } else if (file.endsWith("less") === true) {
+
+                    element = doElement("link");
+                    element.href = file;
+                    element.type = "text/css"
+                    element.rel = 'stylesheet/less';
+                }
+                else if (file.endsWith("js") === true) {
+
                     element = doElement("script");
                     element.src = file;
                 } else if (file.endsWith("ico")) {
+
                     element = doElement("link");
                     element.href = file;
                     element.type = "image/x-icon";
@@ -27,6 +36,7 @@ function Component(content: HeaderContent) {
                 }
 
                 if (element) {
+                    console.log(`${file} was loaded`);
                     head.appendChild(element)
                 }
 
@@ -41,30 +51,62 @@ function doElement<K extends keyof HTMLElementTagNameMap>(name: K) {
 }
 @Component({
     files: [
-        './src/js/vendor/OwlCarousel2-2.2.1/dist/assets/owl.carousel.min.css',
-        './src/js/vendor/OwlCarousel2-2.2.1/dist/assets/owl.theme.default.min.css',
         './src/css/bootstrap.css',
         './src/css/index.css',
-        './src/js/vendor/OwlCarousel2-2.2.1/docs/assets/vendors/jquery.min.js',
+
+        './src/js/less.min.js',
+        './src/js/jquery.min.js',
         './src/js/bootstrap.js',
-        './src/js/angular.min.js',
         './src/img/favicon.ico'
     ]
 })
-
 class Main {
     constructor() {
-        $(() => {
-            $(".owl-carousel").owlCarousel({
-                items: 1,
-                loop: true,
-                autoplay: true,
-                autoplayTimeout: 3000,
-                autoplayHoverPause: true,
-            });
+
+    }
+    init() {
+      
+
+            let app = angular.module("body", ['ng']);
+            app.controller("bodyController", ($scope) => {
+
+                $scope.table = [
+                    { month: "3", monthKey: "个月", rate: "3.50%", total: "1,880" },
+                    { month: "3-6", monthKey: "个月", rate: "3.60%", total: "2,880 " },
+                    { month: "6-12", monthKey: "个月", rate: "4.50%", total: "8,880" },
+                    { month: "12-24", monthKey: "个月", rate: "5.60%", total: "5,880" },
+                    { month: "24", monthKey: "个月以上", rate: "6.50%", total: "91,080" },]
+                $scope.table2 = [
+                    { month: "3", monthKey: "个月", monthNotice: "保健消费", rate: "4.90%", total: "18,000", button: "投标" },
+                    { month: "5", monthKey: "个月", monthNotice: "资金周转", rate: "5.20%", total: "28,880 ", button: "投标" },
+                    { month: "7", monthKey: "个月", monthNotice: "个人消费", rate: "5.50%", total: "38,880", button: "还款中" },
+                    { month: "1", monthKey: "个月", monthNotice: "日常生活消费", rate: "4.60%", total: "59,880", button: "还款中" },]
+                $scope.safe = [
+                    {
+                        name: "安全", values: [
+                            { Record: "国家AAA信用平台" },
+                            { Record: "银行资金托管" },
+                            { Record: "上市公司北京保证" }]
+                    },
+                    {
+                        name: "权威", values: [
+                            { Record: "国家AAA信用平台" },
+                            { Record: "银行资金托管" },
+                            { Record: "上市公司北京保证" }]
+                    },
+                    {
+                        name: "省心", values: [
+                            { Record: "1000元起头" },
+                            { Record: "用户利益保障机制" },
+                            { Record: "保险公司承保" }]
+                    },
+
+                ]
+            })
         })
     }
 }
 
 
-new Main();
+var main = new Main();
+main.init()
